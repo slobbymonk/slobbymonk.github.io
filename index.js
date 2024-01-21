d4358d9bdd01cde162b94567eaf0f40372de5f2f
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2024-01-21 09:07:08 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+function show(){
+  document.getElementById("hiddenContent").style.display = "inline-block";
+  }
+  
+  function hide(){
+  document.getElementById("hiddenContent").style.display = "none";
+  }
