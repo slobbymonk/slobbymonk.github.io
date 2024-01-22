54cb75b7157bd97ddb79466982b771ee21fada4d
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2024-01-22 12:04:25 +0100

    Project hover opens description.
    Made it so that the project scroll list now opens a description when you hover over a project.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-function show(){
-  document.getElementById("hiddenContent").style.display = "inline-block";
+window.onload = function() {
+  hideOnLoad()
+};
+function hideOnLoad(){
+  const collection = document.getElementsByClassName("project-image-description");
+  for (let i = 0; i < collection.length; i++) {
+    collection[i].style.display = "none";
+  }
+}
+
+function show(parent){
+  parent.children[0].style.display = "inline-block";
   }
   
-function hide(){
-document.getElementById("hiddenContent").style.display = "none";
+function hide(parent){
+  parent.children[0].style.display = "none";
 }
