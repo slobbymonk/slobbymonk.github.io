fc4cca0cb6fd17b883885d0b675f5e99e9390309
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2024-01-23 19:35:04 +0100

    The website is now fully working.
    You can move around with your mouse and hover over projects to see a description.

--- index.js
+++ index.js
@@ -1,53 +1,77 @@
 const gallery = document.getElementById("gallery");
-let isMoving = false;
-
-let currentPanX = 0;
-let currentPanY = 0;
-
-let mouseX,
-      mouseY;
 
 window.onmousemove = e => {
-  mouseX = e.clientX,
-  mouseY = e.clientY;
-};
-// document.addEventListener('mouseleave', () => {
-//       isMoving = false;
-// });
-// document.addEventListener('mouseenter', () => {
-//       isMoving = true;
-// });
-document.addEventListener('mousedown', () => {
-      isMoving = true;
-});
-document.addEventListener('mouseup', () => {
-      isMoving = false;
-});
-
-function update(){
-      if(isMoving){
-            const xDecimal = mouseX / window.innerWidth,
+      const mouseX = e.clientX,
+            mouseY = e.clientY;
+      
+      const xDecimal = mouseX / window.innerWidth,
             yDecimal = mouseY / window.innerHeight;
 
-            // const willMove = xDecimal <= 0.05 || xDecimal >= 0.95 || yDecimal <= 0.05 || yDecimal >= 0.95;
+      const maxX = gallery.offsetWidth - window.innerWidth;
+            maxY = gallery.offsetHeight - window.innerHeight;
 
-            // if (!willMove) {
-            //       window.requestAnimationFrame(update);
-            //   return; // Do not update if outside the specified range
-            // }
+      const panX = maxX * xDecimal * -1,
+            panY = maxY * yDecimal * -1;
 
-            const targetPanX = gallery.offsetWidth * xDecimal * -1;
-            const targetPanY = gallery.offsetHeight * yDecimal * -1;
-
-            const easeFactor = 0.01; // Adjust the ease factor for the desired transition speed
-
-            currentPanX += (targetPanX - currentPanX) * easeFactor;
-            currentPanY += (targetPanY - currentPanY) * easeFactor;
-
-            gallery.style.transform = `translate(${currentPanX}px, ${currentPanY}px)`;
-
-
-      }
-      window.requestAnimationFrame(update);
+      gallery.animate({
+        transform: `translate(${panX}px, ${panY}px)`
+      }, {
+            duration: 4000,
+            fill: "forwards",
+            easing: "ease"
+      })
 }
-window.requestAnimationFrame(update);
+
+// const gallery = document.getElementById("gallery");
+// let isMoving = false;
+
+// let currentPanX = 0;
+// let currentPanY = 0;
+
+// let mouseX,
+//       mouseY;
+
+// window.onmousemove = e => {
+//   mouseX = e.clientX,
+//   mouseY = e.clientY;
+// };
+// // document.addEventListener('mouseleave', () => {
+// //       isMoving = false;
+// // });
+// // document.addEventListener('mouseenter', () => {
+// //       isMoving = true;
+// // });
+// document.addEventListener('mousedown', () => {
+//       isMoving = true;
+// });
+// document.addEventListener('mouseup', () => {
+//       isMoving = false;
+// });
+
+// function update(){
+//       if(isMoving){
+//             const xDecimal = mouseX / window.innerWidth,
+//             yDecimal = mouseY / window.innerHeight;
+
+//             // const willMove = xDecimal <= 0.05 || xDecimal >= 0.95 || yDecimal <= 0.05 || yDecimal >= 0.95;
+
+//             // if (!willMove) {
+//             //       window.requestAnimationFrame(update);
+//             //   return; // Do not update if outside the specified range
+//             // }
+
+//             const targetPanX = gallery.offsetWidth * xDecimal * -1;
+//             const targetPanY = gallery.offsetHeight * yDecimal * -1;
+
+//             const easeFactor = 0.01; // Adjust the ease factor for the desired transition speed
+
+//             currentPanX += (targetPanX - currentPanX) * easeFactor;
+//             currentPanY += (targetPanY - currentPanY) * easeFactor;
+
+//             gallery.style.transform = `translate(${currentPanX}px, ${currentPanY}px)`;
+
+
+//       }
+//       window.requestAnimationFrame(update);
+// }
+// window.requestAnimationFrame(update);
