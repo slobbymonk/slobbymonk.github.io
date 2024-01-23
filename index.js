dda52f9f34359e5f47b02a9625ae5c9efe103fe3
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2024-01-23 12:22:27 +0100

    Nvm now I changed it.

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
-window.onload = function() {
-  hideOnLoad()
-};
-function hideOnLoad(){
-  const collection = document.getElementsByClassName("project-image-description");
-  for (let i = 0; i < collection.length; i++) {
-    collection[i].style.display = "none";
-  }
+const gallery = document.getElementById("gallery");
+let canPan = false;
+
+
+document.body.onmousedown = function() { 
+      canPan = true;
+    }
+document.body.onmouseup = function() {
+      canPan = false;
 }
 
-function show(parent){
-  parent.children[0].style.display = "inline-block";
+window.onmousemove = e => {
+  const mouseX = e.clientX,
+        mouseY = e.clientY;
+
+  const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+  
+
+  const panX = gallery.offsetWidth * xDecimal * -1,
+        panY = gallery.offsetHeight * yDecimal * -1;
+    
+  if(canPan){
+      gallery.animate({
+            transform: `translate(${panX}px, ${panY}px)`
+      }, {
+            duration: 4000,
+            fill: "forwards",
+            easing: "ease"
+      })
   }
   
-function hide(parent){
-  parent.children[0].style.display = "none";
 }
+
