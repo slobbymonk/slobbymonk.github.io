542ea2a7498f1b411cfe15a1b12addf2bf6524fc
Slobbymonk <40126809+slobbymonk@users.noreply.github.com>
2024-05-11 02:39:35 +0200

    Started making pages & updates lots of things

---
+++ Navbar.php
@@ -0,0 +1,6 @@
+<ul class="navigation-card">
+  <li class="active"><a href="index.html">Projects</a></li>
+  <li class="tab"><a href="404Page.html">Resume</a></li>
+  <li class="tab"><a href="404Page.html">Contact</a></li>
+  <li class="tab"><a href="AboutMe.html">About Me</a></li>
+</ul>
